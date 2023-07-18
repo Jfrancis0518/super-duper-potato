d9527810a5de601a0a644ee3f0b3117335def733
Jfrancis0518 <jfrancis0518@gmail.com>
2023-07-18 15:36:32 -0400

    update

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,12 @@ const apiKey = 'YOUR_API_KEY';
 const baseUrl ='https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
 
 
-const fetchWeatherData = async (cityname) => {
-    const response = await fetch(url);
-    const data = await response.json();
-}
+const fetchWeatherData = async (cityName) => {
+    try {
+      const url = `${baseUrl}?q=${encodeURIComponent(cityName)}&appid=${apiKey}`;
+      const response = await fetch(url);
+      const data = await response.json();
+    } catch (error) {
+        console.log('Error:' error);
+    }
+};
