d47a32e8cd353a3042af90dc48ba9f475d73231c
Jfrancis0518 <jfrancis0518@gmail.com>
2023-08-01 15:53:34 -0400

    update

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-const apiKey = 'YOUR_API_KEY';
+/*const apiKey = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}';
 const baseUrl ='https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
 
 
@@ -7,7 +7,49 @@ const fetchWeatherData = async (cityName) => {
       const url = `${baseUrl}?q=${encodeURIComponent(cityName)}&appid=${apiKey}`;
       const response = await fetch(url);
       const data = await response.json();
+  
+      const weatherContainer = document.getElementById('weather-container');
+      weatherContainer.innerHTML = `<h2>${cityName}</h2><p>Temperature: ${data.main.temp}</p><p>Humidity: ${data.main.humidity}</p>`;
+  
+      
     } catch (error) {
-        console.log('Error:' error);
+      console.log('Error:', error);
     }
+  };
+  
+
+  document.getElementById('search-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const cityName = document.getElementById('city-input').value;
+    fetchWeatherData(cityName);
+  });
+  
+
+  const weatherContainer = document.getElementById('weather-container');
+weatherContainer.innerHTML = `<h2>${cityName}</h2><p>Temperature: ${temperature}</p><p>Humidity: ${humidity}</p>`;
+*/
+const apiKey = 'YOUR_API_KEY_HERE';
+const baseUrl ='https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+
+const fetchWeatherData = async (cityName) => {
+  try {
+    const url = `${baseUrl}?q=${encodeURIComponent(cityName)}&appid=${apiKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const weatherContainer = document.getElementById('weather-container');
+    weatherContainer.innerHTML = `<h2>${cityName}</h2><p>Temperature: ${data.main.temp}</p><p>Humidity: ${data.main.humidity}</p>`;
+  } catch (error) {
+    console.log('Error:', error);
+  }
 };
+
+document.getElementById('search-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const cityName = document.getElementById('city-input').value;
+  fetchWeatherData(cityName);
+});
+
+
+
+  
